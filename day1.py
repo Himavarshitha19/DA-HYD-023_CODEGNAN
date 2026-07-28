98e47f6efb06e137aebacd388854c171a9b1633b
Himavarshitha19 <himavarshitha14@gmail.com>
2026-07-28 13:42:08 +0530

    Tokens

---
+++ day1.py
@@ -0,0 +1,18 @@
+#print('Hello Codegnan')
+'''
+name = 'Codegnan'
+print(name)
+email_id ='himavarshitha14@gmail.com'
+print(email_id) 
+batch = 'day23'
+print(batch)
+length = 5
+breadth = 10
+area = length*breadth
+print(area)
+'''
+#multiassigment of variables
+name,location,age = 'Codegnan', 'hyd', 7
+print(name)
+print(location)
+print(age)
