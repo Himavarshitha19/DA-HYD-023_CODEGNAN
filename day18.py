676f6236b57331bc82596e0acedd0b2e43a4a1f0
Himavarshitha19 <himavarshitha14@gmail.com>
2026-08-20 17:54:59 +0530

    argskwargs

--- day18.py
+++ day18.py
@@ -1,108 +1,110 @@
 '''
-Functions-->Variable length arguments(*args)
-         -->Keyword Variable length arguments(**kwargs)
-#Variable length arguments-->The number of positional arguments are not limit
-#we can pass any number of positional arguments,but we need to use the *representation,
-#data is stored in tuple.
+functions-->variable length arguments(*args)
+        -->keyword variable length arguments(**kwargs)
 '''
-
-def sample(*args):
-    """Simple demo for args"""
+#1.variable length arguments-->the number of positional arguments are not limit
+#we can pass any number of arguments,but we need to use the * representation,
+#data is stored in tuple
+'''
+def sample(*args): #by * we can pass any positional arguments
+    """Simple demo for *args"""
     print(args)
     print(type(args))
 sample() #no arguments
-sample(1,3,5,6) #any arguments
-sample('Engineer','Varsha',23)
-details = [24,45,35,65]
+sample(1,3,5,6) #any number
+sample('Engineer','varsha',21)
+details=[24,45,35,65] 
 sample(details) #passing a collection
 sample(*details) #unpacking values from collection
-
-a,b,c = 13,4,'da'
+'''
+#--->another example for variable length arguments by using variables
+#'*' is used for unpacking the values into a collection
+'''
+a,b,c=13,4,'da'
 print(a,b,c)
-#a,*b,c = 'python','Engineer',23,45,9.7,'data'
-#a,b,*c = 'python','Engineer',23,45,9.7,'data'
-a,b,*c = 34,'analytics'
+
+#in this i gave * to the variable b so codegnan and the numeric values stored in b in the form of list
+a,*b,c='python','engineer',23,45,6.7,'data'
+print(a)
+print(b)
+print(c)
+
+#in this i gave * to the variable c , so the numberic values and data is stored in c in the form of list
+a,b,*c='python','engineer',23,45,6.7,'data'
+print(a)
+print(b)
+print(c)
+
+#in this we given * to c
+#first in c there is no value so it passed empty list
+#next i had given extend to c and passed some values and printed, so the values will be printed in c
+a,b,*c=34,'codegnan'
 print(a)
 print(b)
 print(c)
 c.extend([23,45,6,7])
 print(c)
-
-
-
-#Task-->We wanted to calculate the sum of given objects using Function
 '''
-def add(*a):
-    """Sum of given objects"""
+
+#Task-->we wanted to calculate the sum of given objects using function
+'''
+ef add(*a):
+    """"Sum of given objects"""
     print(a)
     print(type(a))
     #take output variable as result
-    result  = 0
+    result = 0
     for i in a:
-        print(i)
-        #if type(i) == int or type(i)== float :
+        #if type(i) == int or type(i) == float:
         if type(i) in (int,float,complex):
-           # print(i)
-         result = result + i
+            #print(i)
+            result = result + i
     return result
 #print(add())
 #print(add(12,3,4,5))
-#print(add(1,2,3,4.5))
-#print(add(3,4,5,'poll','dear',45,4.5))
-
-#if user want in dynamic then
-b= list(map(int,input("Enter the values").split(',')))
-print(add(*b)) # * is used to unpack the values from collection 
+#print(add(1,2,3,4,5.6))
+#add(3,4,5,'poll','dear',4.5)hence its the breaks the sum since it contines strings
+#print(add(3,4,5,5.5,2+4j,56.'code',23))
+print(add(3,4,5,'poll','dear',4.5))
+b = list(map(int,input("Enter the values:").split(',')))
+#print(add(*b))#here  '*' is used to unpack the values from collection
 print(b)
-#print(*b) #it returns each value side by side
+print(*b)#it returns each value sde by side
 for i in b:
-    print(i,end=' ') #same as here
+    print(i,end='') #it does the same process here as the "*" do
 '''
 
-
-#Keyword variable length arguments--> we can pass any number of keyword
-#arguments we use ** representation ,data is stored in dictionary
-'''   
-def detaiils(**kwargs):
-    """Usage of**kwargs demo"""
+#2.Keyword variable length arguments-->we can pass any number of keyword arguments
+#as we use ** representation
+#kwargs-->keyword variable length arguments
+#the data is stored in dictionary
+'''
+def details(**kwargs):
+    """usage of **kwargs demo"""
     print(kwargs)
     print(type(kwargs))
-details() #returns empty dictionary
-#details(2,3,4,6) #raises TypeError
-details(name='Engineer',place='hyd',batch='da')
-batch = {'number':'da23','place':'hyd'}
+details() #return empty dictionary
+#details(2,3,4,5)-->wrong representation
+details(name='engineer',place='hyd',batch='da')
+
+batch={'number':'da23','place':'hyd'}
 details(**batch)
 '''
- 
-#--->Now let us include both of them into a function
+
+#--->now let us include both of them into a function(above example)
 '''
 def sample(*a,**b):
-    """Usage of both variable length and keyword variable length args"""
-    result = 0
+    """usage of both varibale length and keyword variable length args"""
+    result=0
     for i in a:
         if type(i) in (int,float,complex):
-            result = result + i
+            result=result+i
     print(result)
     for key,value in b.items():
-        print(f'key is {key}')
-        print(f'value is {value}')
+        print(f'key is : {key}')
+        print(f'Value is : {value}')
 sample(2,4,5,'police','engineer',3.5,
-       name = 'engineer',
-       place = 'hyd',
-       batch='da26')
+              name='engineer',
+              place='hyd',
+              batch='da23')
 '''
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
